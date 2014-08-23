1373b773a11c77683e6ddedf8918a08b1e1c0539
Alisher667 <alisher7@me.com>
2014-08-23 05:08:18 +0000

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
  before_action :signed_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
 
- def create
+  def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
@@ -21,7 +21,11 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-     @micropost = current_user.microposts.find_by(id: params[:id])
+      params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
 end
